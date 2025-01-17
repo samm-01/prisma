c6e4cae2e44ebc80b74736c953ab252f5bb6d829
samm-01 <sameergarg006@gmail.com>
2025-01-17 11:06:44 +0530

    prisma script updated

--- script.ts
+++ script.ts
@@ -3,6 +3,28 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.deleteMany();
+  const user = await prisma.user.create({
+    data: {
+      name: "Kyle",
+      email: "kyle@test.com",
+      age: 25,
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    // include: {
+    //   userPreference: true,
+    // },
+    select: {
+      name: true,
+      // email: true,
+      // age: true,
+      userPreference: true,
+    },
+  });
+  console.log(user);
 }
 
 main()
