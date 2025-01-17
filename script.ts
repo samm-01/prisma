889dd777ffeaa16b520881d127189e18040e9e71
samm-01 <sameergarg006@gmail.com>
2025-01-17 10:11:39 +0530

    Model setup

--- script.ts
+++ script.ts
@@ -2,8 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({ data: { name: "Sameer" } });
-  console.log(user);
+  await prisma.user.deleteMany();
 }
 
 main()
